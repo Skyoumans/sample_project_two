c13007b0e914ea781e1df6ce14d77e8fe86706b1
Skyoumans <skyyoumans@gmail.com>
2017-09-27 14:03:27 -0400

    Added a Companies Index route, Added a snowboard Index Route, schemas and seeds for both

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 require('dotenv').config();
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
-
-const db = mongoose.connection
+mongoose.connect(process.env.MONGODB_URI); 
+const db = mongoose.connection;
 db.on('error', (error) => {
-  console.log(error)
+  console.log("eerror")
 })
 db.once('open', () => {
   console.log('Connected to MongoDB')
 })
-
 var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -34,26 +26,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
-app.use('/users', users);
-
+const companyController = require('./routes/companyController')
+app.use('/companies', companyController)
+const snowboardController = require('./routes/snowboardController')
+app.use('/companies/:companyId/snowboards', snowboardController)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
---
+++ routes/companyController.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const Schema = require("../db/schema.js");
+const CompanyModel = Schema.CompanyModel;
+
+router.get('/', (request, response) => {
+    CompanyModel.find({})
+        .then((companies) => {
+            response.render('companies/index', {
+                companies: companies
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+module.exports = router
